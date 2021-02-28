35dd3d1b51f2fa193ea8c3ea47c2f3544cd2834e
MrYuudai <77799196+MrYuudai@users.noreply.github.com>
2021-02-28 22:21:34 +0300

    Revert "Update index.js"
    This reverts commit 4abc7a44112da44b99aab0d3a8b8f8b33ddac2ad.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', msg => {
 
 bot.on('message', msg => {
     if (msg.content === prefix + 'Команды') {
-        msg.reply(' ``` - !Rem, !Rem кто ты?. ``` ');
+        msg.reply(' ``` - !Rem, !Rem кто ты. ``` ');
     }
 });
 
