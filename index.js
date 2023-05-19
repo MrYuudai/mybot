cdf1b1cf90cb25576439562e1b374ddeab6fbee8
MrYuudai <77799196+MrYuudai@users.noreply.github.com>
2023-05-19 23:22:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,39 +16,72 @@ bot.on('ready', () => {
         console.log(link);
     });
 });
-//команда, и то, что она должна выполнить
-bot.on('message', msg => {
-    if (msg.content === prefix + 'Ты тут?') {
-        msg.reply('Да я тут');
-    }
-});
+# Основной баланс
+main_balance = 0
 
-bot.on('message', msg => {
-    if (msg.content === prefix + 'Rem') {
-        msg.reply('Да сенпай!');
-    }
-});
+# Последнее действие
+last_action = None
 
-bot.on('message', msg => {
-    if (msg.content === prefix + 'Сюда') {
-        msg.reply('Хорошо сенпай');
-    }
-});
+# Предпоследнее действие
+prev_action = None
 
-bot.on('message', msg => {
-    if (msg.content === prefix + '+') {
-        msg.member.roles.remove('801095844044341279').catch(() => {
-            console.error("не удалось удалить роль");
-        });
-    }
-});
+# История снятия и пополнения
+history = []
 
-bot.on('message', msg => {
-    if (msg.content === prefix + '+') {
-        msg.member.roles.add('800330787240673312').catch(() => {
-            console.error("не удалось добавить роль");
-        });
-    }
-});
+# Команда /старт
+@bot.command()
+async def start(ctx):
+    global main_balance
+    global last_action
+    global prev_action
+    
+    # Отправка текста в тот же канал
+    await ctx.send("Основной баланс: {}$".format(main_balance))
+    if last_action:
+        await ctx.send("Последнее действие: {}".format(last_action))
+    if prev_action:
+        await ctx.send("Предпоследнее действие: {}".format(prev_action))
+
+# Команда /история
+@bot.command()
+async def история(ctx):
+    # Отображение истории только для автора команды
+    if ctx.author == ctx.message.author:
+        for i, entry in enumerate(history, start=1):
+            await ctx.author.send("{}. {}".format(i, entry))
+
+# Команда /Пополнение
+@bot.command()
+async def Пополнение(ctx, amount, reason):
+    global main_balance
+    global last_action
+    global prev_action
+    
+    # Добавление записи в историю
+    history.append("{} / Пополнение: {}$ / Причина: {}".format(ctx.author.name, amount, reason))
+    
+    # Обновление баланса
+    main_balance += int(amount)
+    
+    # Обновление последнего и предпоследнего действия
+    prev_action = last_action
+    last_action = "{} / Пополнение: {}$ / Причина: {}".format(ctx.author.mention, amount, reason)
+
+# Команда /Снятие
+@bot.command()
+async def Снятие(ctx, amount, reason):
+    global main_balance
+    global last_action
+    global prev_action
+    
+    # Добавление записи в историю
+    history.append("{} / Снятие: {}$ / Причина: {}".format(ctx.author.name, amount, reason))
+    
+    # Обновление баланса
+    main_balance -= int(amount)
+    
+    # Обновление последнего и предпоследнего действия
+    prev_action = last_action
+    last_action = "{} / Снятие: {}$ / Причина: {}".format(ctx.author.mention, amount, reason)
 
 bot.login(token);
